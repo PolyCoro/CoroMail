661f04115cb8285bbe309b6c3e8c0e7dd1d8af10
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-10 17:05:04 +0100

    Ajout documentation code

--- src/coder.py
+++ src/coder.py
@@ -1,45 +1,38 @@
-
 import sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-
 class Coder :
-	"""
-	This class is used to encrypt a message.
+    """
+    This class is used to encrypt a message.
+
     Attributes:
-        algo (str): encryption algorithm choose.
+        algo (str): encryption algorithm chosen.
 
     """ 
 
-	def __init__(self, algo):
-		""" Fill the class attributes
-		Args:
-			algo (str): encryption algorithm choose."""
-		self.algo = algo
+    def __init__(self, algo):
+        """ Fill the class attributes
+
+        Args:
+            algo (str): encryption algorithm choose."""
+
+        self.algo = algo
  
-	def code(self,text,publickey) :
-		if len(text) == 0 :
-			return "FAIL"
-		if (self.algo == "RSA"):
-			tmp = bytes(text, 'utf-8')
-			encryptor = PKCS1_OAEP.new(publickey)
-			encrypted_msg = encryptor.encrypt(tmp)
-		return encrypted_msg
+    def code(self, text, publickey) :
+        """ Encrypt a message.
 
-
-if __name__ == '__main__':
-	
-	message = "test"
-	key = RSA.generate(1024)
-	public_key = key.publickey()
-	encrypt = Coder("RSA")
-	encrypted_msg = encrypt.code(message,public_key)
-	print(encrypted_msg)
-	decryptor = PKCS1_OAEP.new(key)
-	decoded_msg = decryptor.decrypt(encrypted_msg)
-	decoded_msg = decoded_msg.decode('utf-8')
-	print(decoded_msg)
-
-
-
+        Args:
+            text (str): text to encrypt.
+            publickey (RsaKey): public key
+        
+        Returns:
+            The encypted message
+        """
+        if len(text) == 0 :
+            return "FAIL"
+        if (self.algo == "RSA"):
+            tmp = bytes(text, 'utf-8')
+            encryptor = PKCS1_OAEP.new(publickey)
+            encrypted_msg = encryptor.encrypt(tmp)
+        return encrypted_msg
