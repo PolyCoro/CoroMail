1347291e9e29c274c6e4f682e135b65e2377b811
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-10 16:50:48 +0100

    Correction path test coder

--- tests/test_coder.py
+++ tests/test_coder.py
@@ -3,8 +3,8 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 import sys
-sys.path.append("/home/deac/workworkwork/CoroMail/src")
-from coder import Coder
+import src
+from src.coder import Coder
 
 
 class TestFuncs(unittest.TestCase):
