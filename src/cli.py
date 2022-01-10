01eb19c3f66f372efaa4bb0c28f0c0b0ad3be106
Vincent <vega@localhost.localdomain>
2022-01-10 17:42:49 +0100

    Added tests for the server option

---
+++ src/cli.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Usage:
+	cli.py [-s|--server]
+
+	-h --help     Show this screen.
+	-s --server   Launch a server (default behavior is launching a client)
+
+"""
+import logging
+from docopt import docopt
+
+def main(*args,**kwargs):
+	""" Parse the options and launch the program accordingly 
+		Args:
+		*args (tuple) : Argument passed to the program
+		**kwargs (tuple) : unused
+	""" 
+	# Necessary for pytest automation
+	ARGS = docopt(__doc__,argv=args)
+	return ARGS
+
+if __name__ == '__main__':
+	main()
---
+++ tests/test_cli.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Test cli
+
+Test cases for the CLI of the polycoro project
+
+Todo:
+    * Finish the tests
+
+"""
+
+import pytest
+from src import cli
+
+def test_s_option():
+    """Test the s option in the CLI.
+
+    """
+
+    ret = cli.main("-s")
+    assert ret["-s"]== True
+
+def test_server_option():
+    """Check that the two option ored give the same results.
+
+    """
+    ret = cli.main("--server")
+    assert ret["--server"]== True
+
+
+def test_server_equivalency_to_s():
+    """Check that the two option ored give the same results.
+
+    """
+    ret = cli.main("--server")
+    assert ret["-s"]== True
