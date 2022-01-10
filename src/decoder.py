076b25be44f67daf947dff7625511ba0de5a14b6
EdouardMatheu <edouardmatheu@gmail.com>
2022-01-10 17:51:59 +0100

    added Decoder function

---
+++ src/decoder.py
@@ -0,0 +1,16 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+class Decoder : 
+
+	def __init__(self, algo):
+	    self.algo = algo
+
+
+	def decode(self,message,privatekey):
+
+		if (self.algo == "RSA"):
+			decryptor = PKCS1_OAEP.new(privatekey)
+			decrypted_msg = decryptor.decrypt(message)
+		return decrypted_msg
