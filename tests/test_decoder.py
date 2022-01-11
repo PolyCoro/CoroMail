a825f69302b6e2b5d7fdc0e7cd1bf92377c0720d
EdouardMatheu <edouardmatheu@gmail.com>
2022-01-11 14:45:06 +0100

    minor modifications (separated tests into individual functions)

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -8,29 +8,33 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 def code(text,publickey) :
-		if len(text) == 0 :
-			return "FAIL"
-		tmp = bytes(text, 'utf-8')
-		encrypted_msg = publickey.encrypt(tmp,sys.getsizeof(tmp))
-		return encrypted_msg
+	if len(text) == 0 :
+		return "FAIL"
+	tmp = bytes(text, 'utf-8')
+	encrypted_msg = publickey.encrypt(tmp,sys.getsizeof(tmp))
+	return encrypted_msg
 
 class tests_decoder(unittest.TestCase):	
 
-	def test_decode(self):
+	def test_decode_basic(self) :
 		key = RSA.generate(1024)
-	
-		#chiffrage
 		public_key = key.publickey()
 		encryptor = PKCS1_OAEP.new(public_key)
 		decryptor = Decoder("RSA")
 
-		clear_msg1 = bytes("Hello World", 'utf-8')
+		clear_msg1 = bytes("Hello World").encode('utf-8')
 		encrypted_msg1 = encryptor.encrypt(clear_msg1)
 		decrypted_msg1 = decryptor.decode(encrypted_msg1,key)
 		self.assertEqual(decrypted_msg1, clear_msg1)
 		
+	def test_decode_special(self) :
+		key = RSA.generate(1024)
+		public_key = key.publickey()
+		encryptor = PKCS1_OAEP.new(public_key)
+		decryptor = Decoder("RSA")
+
 		f2 = open('tests/special_caracters.txt', 'r')
-		clear_msg2 = bytes(f2.read(), 'utf-8')
+		clear_msg2 = bytes(f2.read()).encode('utf-8')
 		f2.close()
 		encrypted_msg2 = encryptor.encrypt(clear_msg2)
 		decrypted_msg2 = decryptor.decode(encrypted_msg2, key)
@@ -38,7 +42,7 @@ class tests_decoder(unittest.TestCase):
 
 		""" Invalid test : pycryptodome can't encrypt message that are longer
 			than RSA_modulus - 2 - 2 * HASH. In our case, 86
-
+	def test_decode(self)
 		f3 = open('tests/lorem_ipsum.txt', 'r')
 		clear_msg3 = bytes(f3.read(), 'utf-8')
 		f3.close()
