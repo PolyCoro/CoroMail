cfbb0b9f03334d955c8c21897f1308b367c87651
ClementRoger <clementrger@gmail.com>
2022-01-11 11:05:46 +0100

    Ajout des tests de la classe Config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os.path
 from os import path
 from Crypto.PublicKey import RSA
+import json
 
 #Config constructor
 #Inputs : path to conf file , URL of the CAS , username of the user
@@ -8,37 +9,30 @@ class Config():
 
     def __init__(self , conf_path , CAS_url , username ):
 
+        self.username = ""
+        self.public_key = ""
+        self.private_key = ""
+        self.CAS_url = ""
+
         #if conf file, load infos from it
-        if path.exists( conf_path + "/" + username + ".conf" ):
-            self.conf_path = conf_path + "/" + username + ".conf"
-            file = open(self.conf_path , "r")
-            text = file.readlines()
-            
-            j = 0
-            s = ""
-            for l in (text):
-                if( l == '\n' ):
-                    if( j == 0 ):
-                        self.username = s
-                    elif( j == 1 ):
-                        self.public_key = s
-                    elif( j == 2 ):
-                        self.private_key = s
-                    else:
-                        self.CAS_url = s
-                    j += 1
-                    s = ""
-                else:
-                    s += l            
-                            
-            file.close()
+        if path.exists( conf_path + "/" + username + ".json" ):
+            self.conf_path = conf_path + "/" + username + ".json"
+            data = {}
+            data['infos'] = []
+            with open(self.conf_path) as json_file:
+                data = json.load(json_file)
+                for p in data['infos']:
+                    self.username = p['username']
+                    self.public_key = p['public_key']
+                    self.private_key = p['private_key']
+                    self.CAS_url = p['CAS_url']            
 
         #else, generate keys and create conf file   
-        else:    
-            self.conf_path = conf_path + "/" + username + ".conf" 
+        else:
+            self.conf_path = conf_path + "/" + username + ".json" 
             self.CAS_url = CAS_url
-            self.private_key = self.generate_keys()[0]
-            self.public_key = self.generate_keys()[1]
+            self.private_key = self.generate_keys()[0][29:-26] #delete headers
+            self.public_key = self.generate_keys()[1][32:-30] #delete headers
             self.username = username
             self.create_conf_file()
         
@@ -51,12 +45,17 @@ class Config():
 
     #create conf file and fill it with infos generated before
     def create_conf_file(self):
-        file = open(self.conf_path , "w")
-        file.write( self.username + '\n')
-        file.write( self.public_key + '\n')
-        file.write( self.private_key + '\n')
-        file.write( self.CAS_url + '\n')
-        file.close()
+        data = {}
+        data['infos'] = []
+        data['infos'].append({
+            'username': self.username,
+            'public_key': self.public_key,
+            'private_key': self.private_key,
+            'CAS_url': self.CAS_url
+        })
+
+        with open(self.conf_path , 'w') as file:
+            json.dump(data, file , indent = 4)
 
     #return public key of the user
     def get_public_key(self):
