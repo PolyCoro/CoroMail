b4e17dbc8b60eb11fff656ab9f20a9cbf89e0350
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-11 09:31:46 +0100

    Correction test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,17 +8,19 @@ class TestFuncs(unittest.TestCase):
 
 	def test_creation_file(self):
 		c1 = Config( os.getcwd() , "http://url_du_server.com" , "Clement" )
-		self.assertEqual( path.exists( os.getcwd() + "Clement.conf" ) , True )
+		self.assertEqual( path.exists( os.getcwd() + "/Clement.conf" ) , True )
+		os.remove(os.getcwd() + "/Clement.conf")
 		self.assertEqual( c1.get_conf_path() , os.getcwd() + "/Clement.conf" )
 		self.assertEqual( c1.get_username() , "Clement" )
-		self.assertEqual( len( c1.get_private_key() ) , 1024  )
-		self.assertEqual( len( c1.get_public_key() ) , 1024  )
+		self.assertEqual( len( c1.get_private_key() ) , 1024)
+		self.assertEqual( len( c1.get_public_key() ) , 1024)
 		self.assertEqual( c1.get_CAS_url() , "http://url_du_server.com" )
 
 	def test_load_file(self):
 		c1 = Config( os.getcwd() , "http://url_du_server.com" , "Clement" )
 		c2 = Config( os.getcwd() , "http://url_du_server.com" , "Clement" )
-		self.assertEqual( path.exists( os.getcwd() + "Clement.conf" ) , True )
+		self.assertEqual( path.exists( os.getcwd() + "/Clement.conf" ) , True )
+		os.remove(os.getcwd() + "/Clement.conf")
 		self.assertEqual( c2.get_conf_path() , os.getcwd() + "/Clement.conf" )
 		self.assertEqual( c2.get_username() , "Clement" )
 		self.assertEqual( len( c2.get_private_key() ) , 1024  )
