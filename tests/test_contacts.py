e1b8bf49c56ba805e1e97a8c50abae7ab0da40bf
Vincent <vega@localhost.localdomain>
2022-01-13 10:12:38 +0100

    update auto fixture

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -34,8 +34,9 @@ add_tst_pwd = "pwd"
 add_tst_pkey = "pkey"
 add_tst_ip = "127.0.0.0"
 add_usr = app_user(add_tst_name,add_tst_pwd,add_tst_pkey,add_tst_ip)
-tst_users = [add_usr,tst_usr]
-remove_db = 0
+tst_users = [tst_usr,add_usr]
+need_db = 0
+
 def test_empty_db_name():
     """
 Check that empty or wrong names for the database raise an error
@@ -64,10 +65,11 @@ def test_add_user():
     """
 Check that empty or wrong names for the database raise an error
 """
+    need_db = 1
     ct = contacts(DB_NAME,tst_usr)
     ct.add_user(add_usr)
     query =  "SELECT * FROM "+tst_usr.name+" WHERE "+USERNAME_COL_NAME + " = '" +add_usr.name +"'"
-
+    
     data = ct.cur.execute(query).fetchall()
     usr = app_user(data[0][0],data[0][1],data[0][2],data[0][3])
     assert usr.name == add_tst_name
@@ -79,7 +81,9 @@ def test_add_user_unique():
     """
 Check that each user has a different name
 """
+    need_db = 1
     ct = contacts(DB_NAME,tst_usr)
+    ct.add_user(add_usr)
     with pytest.raises(sqlite3.IntegrityError):
           ct.add_user(add_usr)
 
@@ -88,10 +92,11 @@ def test_get_user():
     """
 Check that empty or wrong names for the database raise an error
 """
-
+    need_db = 1
     ct = contacts(DB_NAME, tst_usr)
-  
+    ct.add_user(add_usr)
     query =  "SELECT * FROM "+tst_usr.name+" WHERE "+USERNAME_COL_NAME + " = '" +add_usr.name +"'"
+
     data = ct.cur.execute( query).fetchall()
     usr = app_user(data[0][0],data[0][1],data[0][2],data[0][3])
     assert usr.name == add_tst_name
@@ -103,9 +108,10 @@ def test_get_users():
     """
 Check that every user in a database are retrieved
 """
-
+    need_db = 1
     ct = contacts(DB_NAME, tst_usr)
     ct.add_user(tst_usr)
+    ct.add_user(add_usr)
     users = ct.get_users()
     i = 0
     for usr in tst_users:
@@ -114,14 +120,17 @@ Check that every user in a database are retrieved
         assert usr.ip  == users[i].ip 
         assert usr.pubkey  ==users[i].pubkey 
         i += 1
-    remove_db = 1
 
 @pytest.fixture(autouse=True)
 def teardown():
+
+    if need_db:
+        ct = contacts(DB_NAME,tst_usr)
     yield
-    if remove_db == 1 :
-        if(os.path.exists(DB_NAME)):
-            os.remove(DB_NAME)
+
+    if(os.path.exists(DB_NAME)):
+        print("I was here")
+        os.remove(DB_NAME)
 
 if __name__ == '__main__':
 
