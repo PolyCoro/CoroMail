b61cd6fde982bb2a4516ef3b0178f9b17f65a48b
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-25 10:23:55 +0100

    Ajout test config (refactoring)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,5 @@
 import src
-from src.config import Config
+from src.config import *
 import unittest
 import os.path
 from os import path
@@ -7,24 +7,51 @@ from os import path
 class TestFuncs(unittest.TestCase):
 
 	def test_creation_file(self):
-		c1 = Config( os.getcwd() , "http://url_du_server.com" , "Clement" )
+		c1 = Config("Clement", os.getcwd())
+		self.assertEqual(c1.config_found, False)
+		c1.create_config("127.0.0.1")
 		self.assertEqual( path.exists( os.getcwd() + "/Clement.json" ) , True )
 		os.remove(os.getcwd() + "/Clement.json")
-		self.assertEqual( c1.get_conf_path() , os.getcwd() + "/Clement.json" )
-		self.assertEqual( c1.get_username() , "Clement" )
-		self.assertEqual( c1.get_CAS_url() , "http://url_du_server.com" )
+		self.assertEqual( c1.filename , os.getcwd() + "/Clement.json" )
+		self.assertEqual( c1.username , "Clement" )
+		self.assertEqual( c1.CAS_url , "127.0.0.1" )
+		self.assertNotEqual(c1.public_key, "")
+		self.assertNotEqual(c1.private_key, "")
 
 	def test_load_file(self):
 		self.assertEqual( path.exists( os.getcwd() + "/Clem.json" ) , False )
-		c1 = Config( os.getcwd() , "http://url_du_server.com" , "Clem" )
+		c1 = Config("Clem", os.getcwd())
+		self.assertEqual(c1.config_found, False)
+		if c1.config_found == False:
+			c1.create_config("localhost")
 		self.assertEqual( path.exists( os.getcwd() + "/Clem.json" ) , True )
-		c2 = Config( os.getcwd() , "http://url_du_server.com" , "Clem" )
+		c2 = Config( "Clem", os.getcwd())
 		os.remove(os.getcwd() + "/Clem.json")
-		self.assertEqual( c2.get_conf_path() , os.getcwd() + "/Clem.json" )
-		self.assertEqual( c2.get_username() , "Clem" )
-		self.assertEqual( c2.get_private_key() , c1.get_private_key()  )
-		self.assertEqual( c2.get_public_key() , c1.get_public_key()  )
-		self.assertEqual( c2.get_CAS_url() , "http://url_du_server.com" )
+		self.assertEqual(c2.config_found, True)
+		self.assertEqual( c2.filename , os.getcwd() + "/Clem.json" )
+		self.assertEqual( c2.username , "Clem" )
+		self.assertNotEqual(c2.public_key, "")
+		self.assertNotEqual(c2.private_key, "")
+		self.assertEqual( c2.CAS_url , "localhost" )
+	
+	def test_no_url(self):
+		#c1 = Config("no_url", os.getcwd() + "/tests")
+		with self.assertRaises(BadConfigError):
+			Config("no_url",os.getcwd() + "/tests")
+
+	def test_no_pk(self):
+		#c1 = Config("no_url", os.getcwd() + "/tests")
+		with self.assertRaises(BadConfigError):
+			Config("no_pk",os.getcwd() + "/tests")
+
+	def test_no_privk(self):
+		#c1 = Config("no_url", os.getcwd() + "/tests")
+		with self.assertRaises(BadConfigError):
+			Config("no_privk",os.getcwd() + "/tests")
+
+	def test_bad_json(self):
+		with self.assertRaises(BadConfigError):
+			Config("bad_json", os.getcwd() + "/tests")
 
 if __name__ == '__main__':
 	unittest.main()
