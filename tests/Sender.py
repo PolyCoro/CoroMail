9a806b75451e982a2e8d61873721bedb5ace2314
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-25 17:59:44 +0100

    Deplacement fichiers tests dans le bon dossier
    Cleanup

---
+++ tests/Sender.py
@@ -0,0 +1,21 @@
+import socket
+from src.coder import Coder
+from src.config import Config
+from base64 import b64decode,b64encode
+from Crypto.PublicKey import RSA
+from src.app_user import AppUser
+
+if __name__ == "__main__":
+    
+    IP = "192.168.69.106"
+    port = 5000
+    cod = Coder("RSA")
+    conf = Config("Clement2" , ".")
+    if not conf.config_found:
+        conf.create_config("localhost")
+    key = RSA.importKey(b64decode(conf.public_key))
+    s = AppUser( IP , port , cod , key )
+    
+    message = "Coucou" 
+    s.send_message( message )
+    s.send_message( 'q' ) #close connection
